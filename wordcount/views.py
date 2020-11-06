b156f5f33df25ee7d8fe675b42ddcc625689d4ce
Hirenkumar Khant <hr266981@dal.ca>
2020-11-06 13:06:47 -0400

    added item sorting for words

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,16 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import re
+import operator
 
 def home(request):
     return render(request, 'home.html')
 
 def count(request):
     fulltext = request.GET['fulltext']
-    wordlist = fulltext.split()
+    filteredtext = re.sub('\W+',' ', fulltext).lower()
+    wordlist = filteredtext.split()
     worddict = dict()
     for word in wordlist:
         if(word not in worddict.keys()):
             worddict[word] = 1
         else:
             worddict[word] += 1
-    return render(request, 'count.html', {'fulltext':fulltext, 'count': len(wordlist), 'worddict':worddict})
+    sorted_items = sorted(worddict.items(), key=operator.itemgetter(1), reverse=True)
+    return render(request, 'count.html', {'fulltext':fulltext, 'count': len(wordlist), 'worddict':sorted_items})

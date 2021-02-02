bcf6dc91f098d225fcc88ca385aff85503736389
Hirenkumar Khant <hiren@Ramyas-MacBook-Pro.local>
2021-02-02 12:38:15 -0400

    created responsive frontend

--- wordcount/views.py
+++ wordcount/views.py
@@ -15,8 +15,10 @@ def home(request):
     filteredtext = re.sub('(\W+)', ' ', fulltext).lower()
     original_spaces = fulltext.count(' ')
     onelinetext = re.sub('\n', ' ', fulltext).lower()
-    print(filteredtext)
     wordlist = onelinetext.split()
+    newpara_pattern = "\n"
+    if(re.search(newpara_pattern, fulltext)):
+        print("new line!!")
     # worddict = dict()
     # for word in wordlist:
     #     if(word not in worddict.keys()):
@@ -31,7 +33,8 @@ def home(request):
     all_total = len(onelinetext) - linebreaks
     char_total = all_total - single_spaces
     paragraphs = all_total-char_total-original_spaces-1
-    return render(request, 'home.html', {'fulltext': fulltext, 'onelinetext':onelinetext, 'original_spaces':original_spaces, 'filteredtext':filteredtext, 'paragraphs':paragraphs, 'all_total': all_total, 'char_total': char_total, 'words_total': words_total})
+    return render(request, 'home.html', {'fulltext': fulltext, 'linebreaks': linebreaks, 'onelinetext': onelinetext, 'original_spaces': original_spaces, 'filteredtext': filteredtext, 'paragraphs': paragraphs, 'all_total': all_total, 'char_total': char_total, 'words_total': words_total})
 
-def about(request):
-    return render(request, 'about.html')
+
+# def about(request):
+#     return render(request, 'about.html')

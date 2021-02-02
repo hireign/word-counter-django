428e7d662724a42096a6c1b326e0f7180de6f5ad
Hirenkumar Khant <hiren@Ramyas-MacBook-Pro.local>
2021-02-02 00:11:58 -0400

    perfect word count charcount with and without space

--- wordcount/views.py
+++ wordcount/views.py
@@ -3,21 +3,35 @@ from django.shortcuts import render
 import re
 import operator
 
-def home(request):
-    return render(request, 'home.html')
+# def home(request):
+#     return render(request, 'home.html')
 
-def count(request):
-    fulltext = request.GET['fulltext']
-    filteredtext = re.sub('\W+',' ', fulltext).lower()
-    wordlist = filteredtext.split()
-    worddict = dict()
-    for word in wordlist:
-        if(word not in worddict.keys()):
-            worddict[word] = 1
-        else:
-            worddict[word] += 1
-    sorted_items = sorted(worddict.items(), key=operator.itemgetter(1), reverse=True)
-    return render(request, 'count.html', {'fulltext':fulltext, 'count': len(wordlist), 'worddict':sorted_items})
+
+def home(request):
+    try:
+        fulltext = request.GET['fulltext']
+    except:
+        fulltext = ""
+    filteredtext = re.sub('(\W+)', ' ', fulltext).lower()
+    original_spaces = fulltext.count(' ')
+    onelinetext = re.sub('\n', ' ', fulltext).lower()
+    print(filteredtext)
+    wordlist = onelinetext.split()
+    # worddict = dict()
+    # for word in wordlist:
+    #     if(word not in worddict.keys()):
+    #         worddict[word] = 1
+    #     else:
+    #         worddict[word] += 1
+    # sorted_items = sorted(
+    #     worddict.items(), key=operator.itemgetter(1), reverse=True)
+    words_total = len(wordlist)
+    linebreaks = fulltext.count('\n')
+    single_spaces = onelinetext.count(' ')
+    all_total = len(onelinetext) - linebreaks
+    char_total = all_total - single_spaces
+    paragraphs = all_total-char_total-original_spaces-1
+    return render(request, 'home.html', {'fulltext': fulltext, 'onelinetext':onelinetext, 'original_spaces':original_spaces, 'filteredtext':filteredtext, 'paragraphs':paragraphs, 'all_total': all_total, 'char_total': char_total, 'words_total': words_total})
 
 def about(request):
     return render(request, 'about.html')

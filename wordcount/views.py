693c82cf541e054d877e9b3f546794b0789ad003
Hirenkumar Khant <hiren@Ramyas-MacBook-Pro.local>
2021-02-03 19:32:36 -0400

    corrected the all_total and char_total, updated css, deleted needless templates

--- wordcount/views.py
+++ wordcount/views.py
@@ -12,13 +12,32 @@ def home(request):
         fulltext = request.GET['fulltext']
     except:
         fulltext = ""
+
     filteredtext = re.sub('(\W+)', ' ', fulltext).lower()
-    original_spaces = fulltext.count(' ')
     onelinetext = re.sub('\n', ' ', fulltext).lower()
+
+    original_spaces = fulltext.count(' ')
+    linebreaks = fulltext.count('\n')
+    single_spaces = onelinetext.count(' ')
+    tabs = onelinetext.count('\t')
+
+
     wordlist = onelinetext.split()
-    newpara_pattern = "\n"
-    if(re.search(newpara_pattern, fulltext)):
-        print("new line!!")
+    words_total = len(wordlist) - tabs
+
+    # number of total characters with white space
+    all_total = len(onelinetext) - linebreaks
+    # number of total characters without white space
+    char_total = all_total - single_spaces - tabs
+
+    # total paragraphs in the input
+    splitedtext = " ".join(onelinetext.split())
+    splited_spaces = splitedtext.count(' ')
+    paragraphs = splited_spaces-original_spaces+1
+
+    # newpara_pattern = "\n"
+    # if(re.search(newpara_pattern, fulltext)):
+    #     print("new line!!")
     # worddict = dict()
     # for word in wordlist:
     #     if(word not in worddict.keys()):
@@ -27,14 +46,5 @@ def home(request):
     #         worddict[word] += 1
     # sorted_items = sorted(
     #     worddict.items(), key=operator.itemgetter(1), reverse=True)
-    words_total = len(wordlist)
-    linebreaks = fulltext.count('\n')
-    single_spaces = onelinetext.count(' ')
-    all_total = len(onelinetext) - linebreaks
-    char_total = all_total - single_spaces
-    paragraphs = all_total-char_total-original_spaces-1
+
     return render(request, 'home.html', {'fulltext': fulltext, 'linebreaks': linebreaks, 'onelinetext': onelinetext, 'original_spaces': original_spaces, 'filteredtext': filteredtext, 'paragraphs': paragraphs, 'all_total': all_total, 'char_total': char_total, 'words_total': words_total})
-
-
-# def about(request):
-#     return render(request, 'about.html')

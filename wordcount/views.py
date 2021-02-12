f1e01e0c2962d810e7afd2c47445af19e1cb1785
Hirenkumar Khant <hiren@Ramyas-MacBook-Pro.local>
2021-02-12 13:04:15 -0400

    updated wordcount charcount nonwhitecharcount with regex for accuracy

--- wordcount/views.py
+++ wordcount/views.py
@@ -13,27 +13,31 @@ def home(request):
     except:
         fulltext = ""
 
-    filteredtext = re.sub('(\W+)', ' ', fulltext).lower()
-    onelinetext = re.sub('\n', ' ', fulltext).lower()
+    # filteredtext = re.sub('(\W+)', ' ', fulltext).lower()
+    # onelinetext = re.sub('\n', ' ', fulltext).lower()
 
-    original_spaces = fulltext.count(' ')
-    linebreaks = fulltext.count('\n')
-    single_spaces = onelinetext.count(' ')
-    tabs = onelinetext.count('\t')
+    # original_spaces = fulltext.count(' ')
+    # linebreaks = fulltext.count('\n')
+    # single_spaces = onelinetext.count(' ')
+    # tabs = onelinetext.count('\t')
 
 
-    wordlist = onelinetext.split()
-    words_total = len(wordlist) - tabs
+    # wordlist = onelinetext.split()
+    # words_total = len(wordlist) - tabs
+    wordlist = re.findall(r'[\w@#$%^&*/\'\’\-_.]+\b', fulltext)
+    words_total = len(re.findall(r'[\w@#$%^&*/\’\'\-_.]+\b', fulltext))
 
     # number of total characters with white space
-    all_total = len(onelinetext) - linebreaks
+    # all_total = len(onelinetext) - linebreaks
+    all_total = len(re.findall('.', fulltext))
     # number of total characters without white space
-    char_total = all_total - single_spaces
+    # char_total = all_total - single_spaces
+    char_total = len(re.findall('\S', fulltext))
 
     # total paragraphs in the input
-    splitedtext = " ".join(onelinetext.split())
-    splited_spaces = splitedtext.count(' ')
-    paragraphs = splited_spaces-original_spaces+1
+    # splitedtext = " ".join(onelinetext.split())
+    # splited_spaces = splitedtext.count(' ')
+    # paragraphs = splited_spaces-original_spaces+1
 
     # newpara_pattern = "\n"
     # if(re.search(newpara_pattern, fulltext)):
@@ -47,4 +51,4 @@ def home(request):
     # sorted_items = sorted(
     #     worddict.items(), key=operator.itemgetter(1), reverse=True)
 
-    return render(request, 'home.html', {'fulltext': fulltext, 'paragraphs': paragraphs, 'all_total': all_total, 'char_total': char_total, 'words_total': words_total})
+    return render(request, 'home.html', {'wordlist':wordlist, 'fulltext': fulltext, 'all_total': all_total, 'char_total': char_total, 'words_total': words_total})
